57b9c5af12eb7dcb49cbe8334363e0fa7688c5b3
Tô Nam Trường <637960@sv.vnua.edu.vn>
2023-08-23 16:46:38 +0700

    update response for BadCredentialsException

--- src/main/java/com/truongto/mock/handlers/GlobalExceptionsHandlers.java
+++ src/main/java/com/truongto/mock/handlers/GlobalExceptionsHandlers.java
@@ -1,13 +1,11 @@
 package com.truongto.mock.handlers;
 
-import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import com.truongto.mock.dtos.ErrorResponse;
 import com.truongto.mock.dtos.ValidationErrorResponse;
-import com.truongto.mock.thfw.exceptions.NotFoundException;
 
 import java.util.Date;
 import java.util.List;
@@ -20,9 +18,9 @@ import org.springframework.validation.FieldError;
 @RestControllerAdvice
 public class GlobalExceptionsHandlers {
 
-        @ExceptionHandler(MethodArgumentNotValidException.class)
+        @ExceptionHandler(org.springframework.web.bind.MethodArgumentNotValidException.class)
         @ResponseStatus(HttpStatus.BAD_REQUEST)
-        public ValidationErrorResponse handleValidationException(MethodArgumentNotValidException ex) {
+        public ValidationErrorResponse handleValidationException(org.springframework.web.bind.MethodArgumentNotValidException ex) {
                 BindingResult bindingResult = ex.getBindingResult();
                 String source = bindingResult.getObjectName();
                 Object target = bindingResult.getTarget();
@@ -50,12 +48,24 @@ public class GlobalExceptionsHandlers {
         }
 
         // NotFoundException
-        @ExceptionHandler(NotFoundException.class)
+        @ExceptionHandler(com.truongto.mock.thfw.exceptions.NotFoundException.class)
         @ResponseStatus(HttpStatus.NOT_FOUND)
-        public ErrorResponse handleNotFoundException(NotFoundException ex) {
+        public ErrorResponse handleNotFoundException(com.truongto.mock.thfw.exceptions.NotFoundException ex) {
                 String source = ex.getStackTrace()[0].getClassName();
                 ErrorResponse errorResponse = new ErrorResponse(source, ex.getMessage(), HttpStatus.NOT_FOUND.value(),
                                 new Date());
+                return errorResponse;
+        }
+
+        // BadCredentialsException
+        @ExceptionHandler(org.springframework.security.authentication.BadCredentialsException.class)
+        @ResponseStatus(HttpStatus.UNAUTHORIZED)
+        public ErrorResponse handleBadCredentialsException(
+                        org.springframework.security.authentication.BadCredentialsException ex) {
+                String source = ex.getStackTrace()[0].getClassName();
+                String message = "Invalid username or password";
+                ErrorResponse errorResponse = new ErrorResponse(source, message,
+                                HttpStatus.UNAUTHORIZED.value(), new Date());
                 return errorResponse;
         }
 
